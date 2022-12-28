6304e349d8eea77f3110ce2c55fb3c1721f1ba4d
mrnguyencoder <mr.nguyencoder@gmail.com>
2022-12-28 20:11:24 +0100

    logo nav

--- src/componants/NavBar.jsx
+++ src/componants/NavBar.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import Logo from '../assets/logoSoniaRachline.png';
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <nav className='flex justify-between p-3'>
+        <div className="">
+            <img className='h-10' src={Logo} alt="Sonia Rachline" />
+        </div>
+        <div className="flex text-xl space-x-3">
+            <p className="">À Propos</p>
+            <p className="">Articles</p>
+            <p className="">Livres</p>
+            <p className="">Contact</p>
+        </div>
+    </nav>
   )
 }
 
