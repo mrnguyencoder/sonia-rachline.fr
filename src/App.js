7df5a385a46bc3304f569182dd3914d5c2281f5a
mrnguyencoder <mr.nguyencoder@gmail.com>
2022-12-28 19:20:15 +0100

    setup componants

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Footer from './componants/Footer';
+import Hero from './componants/Hero';
+import Main from './componants/Main';
+import NavBar from './componants/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div >
+      <NavBar />
+      <Hero />
+      <Main />
+      <Footer />
     </div>
   );
 }
