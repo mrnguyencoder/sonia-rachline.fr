8271186e99a9bb84da15d2f39ff08c08bd0f3b2a
mrnguyencoder <mr.nguyencoder@gmail.com>
2022-12-30 23:23:59 +0100

    footer

--- src/componants/Footer.jsx
+++ src/componants/Footer.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { FiFacebook, FiLinkedin } from "react-icons/fi";
-import { Link } from 'react-router-dom';
 import Logo from '../assets/logoSoniaRachline.png';
 
 
@@ -10,24 +9,31 @@ function Footer() {
       <div className="flex justify-between items-center p-8">
         <div className=''>
           <div>
-            <img src={Logo} alt="" className='h-20' />
+            <img src={Logo} alt="" className='h-20 hover:bg-slate-100' />
           </div>
         </div>
-        <div className="space-y-3">
-          {/* <Link to="/MentionsLegales">Mentions Legales</Link>
-          <Link to="/PolitiqueDeConfidentialite">Politique De Confidentialite</Link> */}
+        <div className="space-y-3 ">
+          <p className='hover:text-green-700'>Mentions Légales </p>
+          <p className='hover:text-yellow-500'>Politique De Confidentialité</p>
         </div>
-        <div className="text-xl flex flex-col space-y-3">
-            <FiLinkedin className=''/>
-            <FiFacebook />
+        <div className="text-3xl flex flex-col space-y-3">
+            <a href="https://www.linkedin.com/in/sonia-rachline-64776630/?originalSubdomain=fr" 
+              className=""
+              rel='noreferrer' target="_blank">
+              <FiLinkedin className='text-[#0e76a8] hover:text-slate-50'/>
+            </a>
+            <a href="https://www.facebook.com/sonia.david.92" className=""
+              rel='noreferrer' target="_blank">
+              <FiFacebook className='text-[#3b5998] hover:text-slate-50'/>
+            </a>
           </div>
       </div>
       <div className="text-center pb-6">
-        <div className="">© 2022 Sonia Rachline | 
+        <div className="hover:text-slate-500">© 2022 Sonia Rachline | 
         <a
           href="https://nguyencoder.com/"
           rel='noreferrer' target="_blank"
-          className="text-slate-400 pl-3"
+          className="text-slate-400 pl-3 hover:text-slate-50"
         >
           Developed by nguyencoder
         </a></div>
