675aebdcb5699cf4dfb9de8fe32ccb73af4d1840
mrnguyencoder <mr.nguyencoder@gmail.com>
2023-01-10 09:46:15 +0100

    footer

--- src/componants/Footer.jsx
+++ src/componants/Footer.jsx
@@ -7,41 +7,36 @@ import Logo from '../assets/logoSoniaRachline.png';
 
 function Footer() {
   return (
-    <footer className='flex flex-col bg-slate-900 text-white p-3'>
-      <div className="flex justify-between items-center py-8 px-2">
-        <div className=''>
-          <div>
-            <img src={Logo} alt="" className='h-20 hover:bg-slate-700 rounded-xl' />
-          </div>
-        </div>
-        <div className="">
-          <Link to="mentions-legales" className='hover:text-green-700'>Mentions Légales</Link>
-        </div>
-        <div className="text-3xl flex flex-col space-y-3">
+    <footer className='flex flex-col bg-slate-800 px-4'>
+      <div className="flex justify-between items-center p-4">
+        <img src={Logo} alt="sonia rachline" className='h-20' />
+        <Link to="mentions-legales" className='text-slate-300 hover:text-green-700'>Mentions Légales</Link>
+        <div className="text-3xl md:text-5xl flex space-x-3">
+            <a href="https://www.facebook.com/sonia.david.92" 
+              className=""
+              rel='noreferrer' target="_blank">
+              <FiFacebook className='text-[#3b5998] hover:text-red-700'/>
+            </a>
             <a href="https://www.linkedin.com/in/sonia-rachline-64776630/?originalSubdomain=fr" 
               className=""
               rel='noreferrer' target="_blank">
-              <FiLinkedin className='text-[#0e76a8] hover:text-slate-50'/>
-            </a>
-            <a href="https://www.facebook.com/" className=""
-              rel='noreferrer' target="_blank">
-              <FiFacebook className='text-[#3b5998] hover:text-slate-50'/>
+              <FiLinkedin className='text-[#0e76a8] hover:text-red-700'/>
             </a>
           </div>
       </div>
       
       <div className="text-center pb-6 md:flex ">
-        <div className="hover:text-slate-500">©{new Date().getFullYear()} Sonia Rachline 
-        </div>
+        <p className="hover:text-red-600 text-slate-400">
+          ©{new Date().getFullYear()} Sonia Rachline 
+        </p>   
         <a
           href="https://nguyencoder.com/"
           rel='noreferrer' target="_blank"
-          className="text-slate-400 hover:text-slate-50"
-        >
-          Developed by nguyencoder
-        </a>
-        
+          className="text-[#61DBFB] hover:text-[#3c873a]">
+          Developed by nguyendev
+        </a>  
       </div>
+
     </footer>
   )
 }
