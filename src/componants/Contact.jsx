a79b459fff7391577c08fe7a27d39bed9d864298
mrnguyencoder <mr.nguyencoder@gmail.com>
2023-01-14 19:39:31 +0100

    contact

--- src/componants/Contact.jsx
+++ src/componants/Contact.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import logo from '../assets/logoSoniaRachline.png';
-import { PhoneIcon } from '@heroicons/react/24/outline';
+import { EnvelopeIcon, PhoneIcon } from '@heroicons/react/24/outline';
 
 
 function Contact() {
   return (
-    <section className="" >
-      <h2 className="">Contact</h2>
-      <div className="">
-        <PhoneIcon  className='h-8'/>
-        <abbr className="">0685421993</abbr>
+    <section className="text-red-600" >
+      <h2 className="text-center text-3xl lg:text-6xl py-10">Contact</h2>
+      <div className="grid grid-cols-1 lg:grid-cols-2 ">
+        <div className="flex flex-col justify-center items-center space-y-6 py-8">
+          <a href="tel:33782885224"
+              className="flex space-x-3 text-lg md:text-xl lg:text-2xl ">
+              <PhoneIcon className="md: h-8 flex-shrink-0 text-green-500" aria-hidden="true" />
+              <span className="md:animate-pulse text-xl lg:text-4xl ">0685421993</span>
+          </a>
+          <a href="mailto:srachine@orange.fr"
+              className="flex space-x-3 text-lg md:text-xl lg:text-2xl ">
+              <EnvelopeIcon className="md: h-8 flex-shrink-0 text-green-500" aria-hidden="true" />
+              <span className="md:animate-pulse text-xl lg:text-4xl">srachine@orange.fr</span>
+          </a>
+        </div>
+        <div className="flex justify-center items-center">
+          <img src={logo} alt="" className='p-10 md:p-20 lg:p-30' />
+        </div>
       </div>
-
-      <img src={logo} alt="" />
     </section>
   )
 }
--- src/componants/Home.jsx
+++ src/componants/Home.jsx
@@ -4,21 +4,23 @@ import React from 'react';
 function Home() {
   return (
     <section className="flex flex-col justify-between items-center space-y-8 lg:space-y-20 p-10 md:p-20 lg:p-32">
-      <div className="-rotate-3 px-10 pt-16 md:pt-0 ">
-        <h1 className="text-4xl text-red-600 hover:animate-ping">Sonia Rachline</h1>
-        <h3 className="text-2xl text-red-800">Rédactrice Journaliste</h3>
-        <p className="text-xl text-slate-100 max-w-4xl pt-5">Des ouvrages d’ici, d’ailleurs, d’art. Des livres portraits, des biographies.</p>
-        <p className="text-xl text-slate-100 max-w-4xl pt-2">Des « Il », des « Elle », des « Eux », et même parfois des « Je ».</p>
-        <p className="text-xl text-slate-100 max-w-4xl pt-2">Une écriture qui s’immisce au fil d’histoires et d’univers singuliers.</p>
-        <p className="text-xl text-slate-100 max-w-4xl pt-2">Un style au service des autres, partenariats avec des éditeurs, des maisons, des personnalités. Le plaisir de la découverte.</p>
-        <p className="text-xl text-slate-100 max-w-4xl pt-2">Et du travail de groupe.</p>
-        <p className="text-xl text-slate-100 max-w-4xl pt-2"></p>
-      </div>
-      <div className="-rotate-3 px-10 ">
-        <h2 className="text-4xl text-red-600 hover:animate-ping">Sonia David</h2>
-        <h3 className="text-2xl text-red-800">Écrivain</h3>
-        <p className="text-xl text-slate-100 max-w-4xl pt-5">Deux romans. Un troisième en devenir. Une petite oeuvre au long cours.</p>
-        <p className="text-xl text-slate-100 max-w-4xl pt-2">À la recherche de réponses. À la poursuite de ce que l’on oublie. Des récits de vrai et de faux.</p>
+      <div className="grid grid-cols-1 lg:grid-cols-2">
+        <div className="-rotate-3 px-10 pt-16 md:pt-0">
+          <h1 className="text-4xl text-red-600 hover:animate-ping">Sonia Rachline</h1>
+          <h3 className="text-2xl text-red-800">Rédactrice Journaliste</h3>
+          <p className="text-xl text-slate-100 max-w-4xl pt-5">Des ouvrages d’ici, d’ailleurs, d’art. Des livres portraits, des biographies.</p>
+          <p className="text-xl text-slate-100 max-w-4xl pt-2">Des « Il », des « Elle », des « Eux », et même parfois des « Je ».</p>
+          <p className="text-xl text-slate-100 max-w-4xl pt-2">Une écriture qui s’immisce au fil d’histoires et d’univers singuliers.</p>
+          <p className="text-xl text-slate-100 max-w-4xl pt-2">Un style au service des autres, partenariats avec des éditeurs, des maisons, des personnalités. Le plaisir de la découverte.</p>
+          <p className="text-xl text-slate-100 max-w-4xl pt-2">Et du travail de groupe.</p>
+          <p className="text-xl text-slate-100 max-w-4xl pt-2"></p>
+        </div>
+        <div className="-rotate-3 px-10 ">
+          <h2 className="text-4xl text-red-600 hover:animate-ping">Sonia David</h2>
+          <h3 className="text-2xl text-red-800">Écrivain</h3>
+          <p className="text-xl text-slate-100 max-w-4xl pt-5">Deux romans. Un troisième en devenir. Une petite oeuvre au long cours.</p>
+          <p className="text-xl text-slate-100 max-w-4xl pt-2">À la recherche de réponses. À la poursuite de ce que l’on oublie. Des récits de vrai et de faux.</p>
+        </div>
       </div>
       {/* Section loop marquee text */}
       <div class="relative flex overflow-x-hidden text-red-700">
