5b0d18deb0d3eb8aedea3c417c9a9e9fbebd3439
mrnguyencoder <mr.nguyencoder@gmail.com>
2023-01-14 17:08:42 +0100

    contact

--- src/componants/Contact.jsx
+++ src/componants/Contact.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import logo from '../assets/logoSoniaRachline.png';
+import { PhoneIcon } from '@heroicons/react/24/outline';
+
 
 function Contact() {
   return (
     <section className="" >
       <h2 className="">Contact</h2>
+      <div className="">
+        <PhoneIcon  className='h-8'/>
+        <abbr className="">0685421993</abbr>
+      </div>
+
+      <img src={logo} alt="" />
     </section>
   )
 }
