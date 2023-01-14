421546317c95f2126d78d37519292144e9a861ff
mrnguyencoder <mr.nguyencoder@gmail.com>
2023-01-14 15:57:12 +0100

    text sonia

--- src/componants/Home.jsx
+++ src/componants/Home.jsx
@@ -7,12 +7,18 @@ function Home() {
       <div className="-rotate-3 px-10 pt-16 md:pt-0 ">
         <h1 className="text-4xl text-red-600 hover:animate-ping">Sonia Rachline</h1>
         <h3 className="text-2xl text-red-800">Rédactrice Journaliste</h3>
-        <p className="text-xl text-slate-100 max-w-4xl pt-5">Sonia Rachline est l'auteure de deux romans Les Petits Succès sont un désastre et David Bowie n'est pas mort (Robert Laffont, 2012 et 2017).</p>
+        <p className="text-xl text-slate-100 max-w-4xl pt-5">Des ouvrages d’ici, d’ailleurs, d’art. Des livres portraits, des biographies.</p>
+        <p className="text-xl text-slate-100 max-w-4xl pt-2">Des « Il », des « Elle », des « Eux », et même parfois des « Je ».</p>
+        <p className="text-xl text-slate-100 max-w-4xl pt-2">Une écriture qui s’immisce au fil d’histoires et d’univers singuliers.</p>
+        <p className="text-xl text-slate-100 max-w-4xl pt-2">Un style au service des autres, partenariats avec des éditeurs, des maisons, des personnalités. Le plaisir de la découverte.</p>
+        <p className="text-xl text-slate-100 max-w-4xl pt-2">Et du travail de groupe.</p>
+        <p className="text-xl text-slate-100 max-w-4xl pt-2"></p>
       </div>
       <div className="-rotate-3 px-10 ">
         <h2 className="text-4xl text-red-600 hover:animate-ping">Sonia David</h2>
         <h3 className="text-2xl text-red-800">Écrivain</h3>
-        <p className="text-xl text-slate-100 max-w-4xl pt-5">Sonia David, de son vrai nom, Sonia Rachline, est rédactrice journaliste, notamment pour 'Vogue' depuis 25 ans</p>
+        <p className="text-xl text-slate-100 max-w-4xl pt-5">Deux romans. Un troisième en devenir. Une petite oeuvre au long cours.</p>
+        <p className="text-xl text-slate-100 max-w-4xl pt-2">À la recherche de réponses. À la poursuite de ce que l’on oublie. Des récits de vrai et de faux.</p>
       </div>
       {/* Section loop marquee text */}
       <div class="relative flex overflow-x-hidden text-red-700">
