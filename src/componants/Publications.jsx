5bcf15246478524767048a863e771185c09d7f2c
mrnguyencoder <mr.nguyencoder@gmail.com>
2023-01-19 17:09:23 +0100

    design

--- src/componants/Publications.jsx
+++ src/componants/Publications.jsx
@@ -221,9 +221,9 @@ function Publications() {
             <p className="">Éditeur: {selectedBook.editeur}</p>
             <p className="">Date de publication: {selectedBook.datePublished}</p>
             <p className="px-6">Resume: {selectedBook.resume}</p>
-            <div className="md:flex space-x-5 space-y-5 justify-center items-center">
-              <button onClick={() => setSelectedBook(null)} className="flex items-center bg-sky-900 rounded-full px-6 py-2"><ArrowLeftCircleIcon className="h-10 text-slate-50"/> Retour à la liste des livres</button>
-              <a href={selectedBook.buyLink} target="_blank" rel="noreferrer" className="flex justify-center items-center px-6 py-2 shadow-lg bg-sky-900 rounded-full hover:animate-pulse">Lien d'achat du livre <ArrowRightCircleIcon className="h-10 text-yellow-300" /> </a>
+            <div className="flex space-x-5 justify-center items-center">
+              <button onClick={() => setSelectedBook(null)} className="flex justify-center items-center bg-sky-900 rounded-full px-4 py-2"><ArrowLeftCircleIcon className="h-10 text-slate-50"/> Retour à la liste des livres</button>
+              <a href={selectedBook.buyLink} target="_blank" rel="noreferrer" className="flex justify-center items-center px-6 py-2 bg-yellow-500 rounded-full hover:animate-pulse">Lien d'achat du livre <ArrowRightCircleIcon className="h-10 text-teal-600 animate-spin" /> </a>
             </div>
             
           </div>
@@ -239,13 +239,13 @@ function Publications() {
           <div className=''>
             <h3 className='text-3xl text-center'>{group.author.name}</h3>
             <div className="flex justify-center items-center text-xl">
-              <ul className='p-8 grid md:grid-cols-2 gap-y-2 gap-x-20 lg:gap-x-32'>
+              <ul className='p-8 space-y-2'>
                 {group.books.map(book => (
                   <li key={book.id}>
                     <Link
                       onClick={() => setSelectedBook(book)}
                       to={`/publications/${book.title}`}
-                      className="hover:border-b">
+                      className="hover:border-b flex justify-center items-center">
                         {book.name}
                     </Link>
                   </li>
