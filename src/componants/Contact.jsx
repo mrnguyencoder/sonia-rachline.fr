97ee22a9332b12bbd9dd658f4673bec3830761a7
mrnguyencoder <mr.nguyencoder@gmail.com>
2023-01-19 12:21:30 +0100

    contact

--- src/componants/Contact.jsx
+++ src/componants/Contact.jsx
@@ -1,39 +1,50 @@
 import React from 'react';
 import logo from '../assets/logoSoniaRachline.png';
 import { EnvelopeIcon, PhoneIcon } from '@heroicons/react/24/outline';
+import balelio from '../assets/babelio.png';
+import amazon from '../assets/amazonfr.png';
+import Flammarion from '../assets/flammarion.png';
+import fnac from '../assets/fnac.png';
 
 
 function Contact() {
   return (
-    <section className="text-red-600 bg-gradient-to-t-primary-blue-400 " >
-      <h2 className="text-center text-3xl lg:text-6xl py-10">Contact</h2>
-      <div className="grid grid-cols-1 lg:grid-cols-2 ">
-        <div className="flex flex-col justify-center items-center space-y-6 py-8">
+    <section className="text-red-700 bg-gradient-to-t-primary-blue-400 " >
+      <h2 className="text-center tracking-widest text-4xl lg:text-6xl py-10">Contact</h2>
+      <div className="grid grid-cols-1 lg:grid-cols-2 md:gap-8">
+        <div className="flex flex-col justify-center items-center space-y-8 py-6">
           <a href="tel:33782885224"
               className="flex space-x-3 text-lg md:text-xl lg:text-2xl ">
-              <PhoneIcon className="md: h-10 flex-shrink-0 text-green-500" aria-hidden="true" />
-              <span className="md:animate-pulse text-xl lg:text-4xl ">0685421993</span>
+              <PhoneIcon className="md: h-10 flex-shrink-0 text-red-600" aria-hidden="true" />
+              <span className="text-xl lg:text-4xl ">0685421993</span>
           </a>
           <a href="mailto:srachine@orange.fr"
               className="flex space-x-3 text-lg md:text-xl lg:text-2xl ">
-              <EnvelopeIcon className="md: h-10 flex-shrink-0 text-green-500" aria-hidden="true" />
-              <span className="md:animate-pulse text-xl lg:text-4xl">srachine@orange.fr</span>
+              <EnvelopeIcon className="md: h-10 flex-shrink-0 text-red-600" aria-hidden="true" />
+              <span className="text-xl lg:text-4xl">srachine@orange.fr</span>
           </a>
+          <div className="grid grid-cols-2 gap-4">
+            <a href="https://www.amazon.fr/Sonia-Rachline/e/B004MO9HTU%3Fref=dbs_a_mng_rwt_scns_share" className="" 
+            rel='noreferrer' target="_blank">
+              <img src={amazon} alt="" className='rounded-lg h-12 w-36 ' />
+            </a>
+            <a href="https://editions.flammarion.com/Auteurs/rachline-sonia" className="" 
+            rel='noreferrer' target="_blank">
+              <img src={Flammarion} alt="" className='rounded-lg h-12 w-36' />
+            </a>
+            <a href="https://www.fnac.com/ia833855/Sonia-Rachline" className="" 
+            rel='noreferrer' target="_blank">
+              <img src={fnac} alt="" className='rounded-lg h-12 w-36' />
+            </a>
+            <a href="https://www.babelio.com/auteur/Sonia-David/208280" className="" 
+            rel='noreferrer' target="_blank">
+              <img src={balelio} alt="" className='rounded-lg h-12 w-36' />
+            </a>
+          </div>
         </div>
         <div className="flex justify-center items-center">
-          <img src={logo} alt="sonia rachline" className='p-10 md:p-20 lg:p-30' />
+          <img src={logo} alt="sonia rachline" className='p-10 md:px-20 lg:px-30' />
         </div>
-        {/* <form 
-            action="https://formsubmit.co/your@email.com" method="POST"
-            className='flex flex-col space-y-4 px-10 pb-10 md:px-20 lg:px-60'>
-           <input type="text" name="nom" placeholder='votre nom et prénom' className='px-6 py-4 outline-none rounded-lg bg-slate-200 border-b border-slate-500 hover:border-slate-900' /> 
-           <input type="number" placeholder='telephone ' name="phone" id="" className='px-6 py-4 outline-none rounded-lg bg-slate-200 border-b border-slate-500 hover:border-slate-900' />
-           <input type="email" name="email" placeholder='email'  id="" className='px-6 py-4 outline-none rounded-lg bg-slate-200 border-b border-slate-500 hover:border-slate-900'/>
-           <textarea name="message" id="" cols="30" rows="10" placeholder='message' className='px-6 py-4 outline-none rounded-lg bg-slate-200 border-b border-slate-500 hover:border-slate-900'></textarea>
-           <div className="flex justify-center">
-            <button type="submit" className='bg-emerald-600 px-6 py-3 text-slate-100 text-lg rounded-full w-36 ring-2 ring-teal-400 hover:bg-emerald-700'>Envoyer</button>
-            </div>
-        </form> */}
       </div>
 
     </section>
