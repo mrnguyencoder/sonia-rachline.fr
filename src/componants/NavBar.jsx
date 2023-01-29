74c4e7f7af446e385c2f74c7181d60cdfa2418ca
mrnguyencoder <mr.nguyencoder@gmail.com>
2023-01-29 15:46:47 +0100

    new navbar

--- src/componants/NavBar.jsx
+++ src/componants/NavBar.jsx
@@ -1,54 +1,49 @@
 import React from 'react';
-import Logo from '../assets/textsonia.png';
-import { Menu } from '@headlessui/react';
-import { Fragment } from 'react';
 import { Link } from 'react-router-dom';
-import { Bars3Icon } from '@heroicons/react/24/outline';
+import Bars3Icon from '@heroicons/react/24/outline/Bars3Icon';
+import Logo from '../assets/textsonia.png';
 
 function NavBar() {
-    const links = [
-        { href: '/', label: 'Accueil', },
-        { href: '/publications', label: 'Publications', },
-        { href: '/contact', label: 'Contact',},
-      ]
+  const [showLinks, setShowLinks] = React.useState(false);
+  const links = [
+    { href: '/', label: 'Accueil', },
+    { href: '/publications', label: 'Publications', },
+    { href: '/contact', label: 'Contact',},
+  ];
+
   return (
     <nav className='flex justify-between p-3 shadow-sm bg-stone-800 text-red-700'>
-        <Link to="/" className="">
-            <img className='h-10 md:h-14 hover:scale-110 transition duration-700' src={Logo} alt="Sonia Rachline" />
-        </Link>
-        <div className="hidden md:flex items-center text-2xl space-x-5 text-red-600 pr-4">
-            <Link to="/" className="hover:animate-pulse" >Accueil</Link>
-            <Link to="/publications" className="hover:animate-pulse" >Publications</Link>
-            <Link to="/contact" className="hover:animate-pulse" >Contact</Link>
-        </div>
-        {/* Menu mobile */}
-        <Menu as="div" className="md:hidden text-right">
-            <Menu.Button>
-                <Bars3Icon className='h-10 hover:scale-110 translate duration-300 hover:bg-transparent'/>
-            </Menu.Button>
-            <Menu.Items className="flex flex-col text-2xl space-y-8 pt-4 pr-3 hover:bg-transparent">
-                {links.map((link) => (
-                /* Use the `active` state to conditionally style the active item. */
-                <Menu.Item key={link.href} as={Fragment}>
-                    {({ active }) => (
-                    <Link
-                        to={link.href}
-                        className={`${
-                        active ? 'hover:scale-105 transition duration-300 bg-transparent hover:bg-transparent' : ' text-red-600 bg-transparent hover:bg-transparent'
-                        }`}
-                    >
-                        {link.label}
-                    </Link>
-                    )}
-                </Menu.Item>
-                ))}
-            </Menu.Items>
-        </Menu>
-
-
-
+      <Link to="/" className="">
+        <img className='h-10 md:h-14 hover:scale-110 transition duration-700' src={Logo} alt="Sonia Rachline" />
+      </Link>
+      <div className="hidden md:flex items-center text-2xl space-x-5 text-red-600 pr-4">
+        <Link to="/" className="hover:animate-pulse" >Accueil</Link>
+        <Link to="/publications" className="hover:animate-pulse" >Publications</Link>
+        <Link to="/contact" className="hover:animate-pulse" >Contact</Link>
+      </div>
+      {/* Menu mobile */}
+      <div className="md:hidden">
+        <Bars3Icon 
+          className='h-10 hover:scale-110 hover:text-red-300 translate duration-300' 
+          onClick={() => setShowLinks(!showLinks)} 
+        />
+        {showLinks && (
+          <div className="absolute flex flex-col right-0 shadow-lg text-2xl space-y-8 p-6 bg-red-800 mr-1 rounded-2xl">
+            {links.map(link => (
+              <Link 
+                key={link.href} 
+                to={link.href} 
+                className="text-red-100 hover:scale-110 transition duration-500"
+                onClick={() => setShowLinks(false)} 
+              >
+                {link.label}
+              </Link>
+            ))}
+          </div>
+        )}
+      </div>
     </nav>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
